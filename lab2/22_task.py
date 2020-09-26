e3a73fc622db5cc7c61cefa55d32aaa6beb503f1
volkir31 <62487523+volkir31@users.noreply.github.com>
2020-09-26 13:15:53 +0300

    Update 22_task.py

--- lab2/22_task.py
+++ lab2/22_task.py
@@ -1,6 +1,3 @@
-# from random import randint
-# from numpy import dot
-
 input_str = input().split()
 count_str1, count_column1_count_str2, count_column2 = int(input_str[0]), int(input_str[1]), int(input_str[2])
 list1 = []
@@ -19,6 +16,9 @@ for i in range(count_str1):
     for j in range(count_column2):
         str1 += f'{list3[i][j]} '
     print(str1)
+    
+# from random import randint
+# from numpy import dot
 
 # for j in range(count_column1_count_str2):
 #     for k in range(count_column2):
