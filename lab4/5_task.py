de86f32d130b270e7a80556769fdadddf3a87019
volkir31 <egor_mixajlichenko@mail.ru>
2020-10-27 22:50:10 +0300

    refactoring

--- lab4/5_task.py
+++ lab4/5_task.py
@@ -12,20 +12,3 @@ input_list.sort(key=lambda list_item: (-list_item[1], list_item[0]))
 for i in input_list:
     print(i[0])
 
-
-
-
-    # if input_str[i] not in input_list:
-        # input_list.append((input_str.count(input_str[i]), input_str[i]))
-    # else:
-    #     continue
-# sorted_list = sorted(set(input_list), reverse=True)
-# print(sorted_list)
-# for i in sorted_list:
-#     for j in sorted_list[-1:sorted_list.index(i):-1]:
-#         if i[0] > j[0]:
-#             print(i[1])
-#             break
-#         elif i[0] == j[0]:
-#             print(j[1])
-#             break
