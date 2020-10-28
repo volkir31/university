1ecfd3a51bd9fd2a0f7cf86dbc4db6804c8799b2
volkir31 <egor_mixajlichenko@mial.ru>
2020-10-28 19:49:12 +0300

    refactoring

--- lab4/7_task.py
+++ lab4/7_task.py
@@ -7,7 +7,6 @@ for i in lines:
     if input_str[0] not in buyers_dict:
         buyers_dict[input_str[0]] = {input_str[1]: int(input_str[2])}
     else:
-<<<<<<< HEAD
         if input_str[1] not in buyers_dict[input_str[0]]:
             buyers_dict[input_str[0]][input_str[1]] = int(input_str[2])
         else:
@@ -17,15 +16,3 @@ for item in sorted_list:
     print(item[0]+':')
     for product, count in sorted(item[1].items()):
         print(product, count)
-=======
-        buyers_dict[input_str[0]] += input_str[1:]
-for name, products in buyers_dict.items():
-    print(name+':')
-    count_iter = 0
-    for product in products:
-        print(product, end=' ')
-        count_iter += 1
-        if count_iter == 2:
-            print('')
-            count_iter = 0
->>>>>>> 011d1f574595be3f269bab82deb5efea37b01536
